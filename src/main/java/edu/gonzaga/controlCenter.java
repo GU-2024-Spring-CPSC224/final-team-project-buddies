070208aa9df1dc31895f7edb619a8ba5f2394007
jguonathan <123508944+jguonathan@users.noreply.github.com>
2024-04-28 23:08:23 -0700

    Add files via upload

--- src/main/java/edu/gonzaga/controlCenter.java
+++ src/main/java/edu/gonzaga/controlCenter.java
@@ -9,8 +9,44 @@ import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import javax.swing.JOptionPane;
+import javax.swing.*;
+
 public class controlCenter implements ActionListener, MouseListener {
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() instanceof JButton) {
+            JButton button = (JButton) e.getSource();
+            if ("Start Race".equals(button.getText())) {
+                // 在这里添加开始游戏的代码
+            } else if ("Reset Race".equals(button.getText())) {
+                // 在这里添加重置游戏的代码
+            }
+        }
+    }
 
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        // 在这里添加你的代码
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // 在这里添加你的代码
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // 在这里添加你的代码
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // 在这里添加你的代码
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // 在这里添加你的代码
+    }
 }
