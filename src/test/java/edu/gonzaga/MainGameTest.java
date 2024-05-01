3687342bb4118f275aa514406abce54ca3f45564
jguonathan <123508944+jguonathan@users.noreply.github.com>
2024-05-01 03:03:10 -0700

    Test for miles choosing

--- src/test/java/edu/gonzaga/MainGameTest.java
+++ src/test/java/edu/gonzaga/MainGameTest.java
@@ -1,14 +1,25 @@
 package edu.gonzaga;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
+import static org.junit.jupiter.api.Assertions.*;
+import edu.gonzaga.view.MainView;
 
 public class MainGameTest {
     @Test
-    void alwaysTrue() {
-        Assertions.assertTrue(true);
+    void testMain4Miles() {
+        App.main(new String[]{"4"});
+        assertNotNull(MainView.getInstance());
+    }
+
+    @Test
+    void testMain6Miles() {
+        App.main(new String[]{"6"});
+        assertNotNull(MainView.getInstance());
+    }
+
+    @Test
+    void testMain8Miles() {
+        App.main(new String[]{"8"});
+        assertNotNull(MainView.getInstance());
     }
 }
